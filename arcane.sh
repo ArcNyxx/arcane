d43fb263cdf60e4ec3b372cdd8d3125fefedd1df
Doggo <fearlessdoggo21@vivaldi.net>
2022-06-18 10:11:39 -0500

    fix permissions collection in install script

--- arcane.sh
+++ arcane.sh
@@ -6,15 +6,14 @@
 # get the qmk firmware required to build arcane from zsa
 # install the teensy loader cli for interfacing with the keyboard
 git clone --recurse-submodules https://github.com/zsa/qmk_firmware/
-pacman -S teensy_loader_cli
+rd pacman -S teensy_loader_cli
 
 # downgrade avr-gcc to 8.3.0 if necessary
 [ "$(avr-gcc --version | sed '1q' | cut '-d ' -f3 | cut -d. -f1)" -gt 8 ] &&
-	pacman -U https://archive.archlinux.org/packages/a/avr-gcc/\
-	avr-gcc-8.3.0-1-x86_64.pkg.tar.xz
+	rd pacman -U https://archive.archlinux.org/packages/a/avr-gcc/avr-gcc-8.3.0-1-x86_64.pkg.tar.xz
 
 # install flashing udev rules
-qmk_firmware/util/udev/50-qmk.rules /etc/udev/rules.d/
+rd qmk_firmware/util/udev/50-qmk.rules /etc/udev/rules.d/
 
 # clone arcane, move it to the proper location, build, and flash
 git clone https://github.com/arcnyxx/arcane/
